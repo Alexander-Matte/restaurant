7f12ac3dbb0d367d30caa6a4b9bdab0769c89c68
Alex Matte <alex.matte71@gmail.com>
2023-03-19 12:26:17 +0100

    adjusted responsiveness on contact page. Added contact info hardcode

--- src/js_modules/contact_init.js
+++ src/js_modules/contact_init.js
@@ -1,19 +1,32 @@
 const contentSection = document.querySelector("#content-section")
 
-const contactContent = `<div class="contact-content d-flex flex-column align-items-center">
-                            <h2>Contact Us</h2>
-                            <form id="contactForm" class="d-flex flex-column" action="#" method="post">
-                                <label for="name">Name:</label>
-                                <input type="text" id="name" name="name" required><br>
+const contactContent = `<div class="contact-content">
+                            <div class="row">
+                                <div class="our-info text-center col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <h2 class="text-center">Contact Us</h2>
+                                    <p>Location: 123 Wing Street</p>
+                                    <p>Telephone: 1(234)567-8910</p>
+                                    <p>Email: wing.world@gmail.com</p>
+                                </div>
 
-                                <label for="email">Email:</label>
-                                <input type="email" id="email" name="email" required><br>
+                                <div class="form-container d-flex justify-content-center col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                                    <form id="contactForm" class="d-flex flex-column w-50" action="#" method="post">
+                                        <h2>Write Us</h2>
+                                        <label for="name">Name:</label>
+                                        <input type="text" id="name" name="name" required><br>
 
-                                <label for="message">Message:</label>
-                                <textarea id="message" name="message" required></textarea><br>
+                                        <label for="email">Email:</label>
+                                        <input type="email" id="email" name="email" required><br>
 
-                                <input class="mt-2" type="submit" value="Submit">
-                            </form>
+                                        <label for="message">Message:</label>
+                                        <textarea id="message" name="message" required></textarea><br>
+
+                                        <input class="mt-2" type="submit" value="Submit">
+                                    </form>
+                                </div>
+                            
+                            </div>
+
                         </div>
 
 `
